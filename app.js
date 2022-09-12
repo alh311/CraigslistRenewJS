34bf97dad720ec4af0816983aa604cca1b0dcefe
Alex H <alh311@gmail.com>
2022-09-11 23:49:30 -0600

    Adds renew functionality * Closes about:blank page * Checks for auth error * Get all display buttons (should be renew buttons eventually) and shift+clicks them * Print item descriptions from each page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
+const loginUrl = 'https://accounts.craigslist.org/login';
+
 (async () => {
     await run();
 })();
@@ -8,9 +10,13 @@ const puppeteer = require('puppeteer');
 async function run() {
 
     // Init the browswer, page, and goto initial page
-    const browser = await puppeteer.launch({ headless: false, });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://accounts.craigslist.org/login');
+    await page.goto(loginUrl);
+
+    // Close all pages besides the one we want (e.g. about:blank)
+    let itemPages = (await browser.pages()).filter(p => p != page);
+    itemPages.forEach(async p => await p.close());
 
     // Login page
     logPage(await page.title());
@@ -21,15 +27,47 @@ async function run() {
         page.click('#login')
     ]);
 
+    // Auth Error
+    // TODO: better error message and better code structure.
+    const hasError = await page.$eval('.submit-onetime-link-button', () => true).catch(() => false);
+    if (hasError) {
+        console.log('There was an error authenticating.')
+        await browser.close();
+        return;
+    }
 
+    // Account page
     logPage(await page.title());
+    const renewButtons = await page.$$('input[value="display"][type="submit"]');
 
+    let count = 0;
+    await page.keyboard.down('Shift');
+    for (const button of renewButtons) {
+        await button.click();
+        ++count;
+        if (count >= 5) {
+            break;
+        }
+    }
+    await page.keyboard.up('Shift');
 
+    // TODO: find a better way to wait on the pages to open before asking browser for all the pages.
+    await sleep(1000);
 
+    itemPages = (await browser.pages()).filter(p => p != page);
+
+    console.log(`Pages: ${itemPages.length}`)
+    for (const p of itemPages) {
+        console.log(await p.$eval('#titletextonly', el => el.innerText));
+    }
 
     await browser.close();
 }
 
 function logPage(title) {
     console.log(`Page: ${title}`);
+}
+
+function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
 }
