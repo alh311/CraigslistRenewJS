5eeb2b3debe99298ca04b34c5d383e9f38890f32
Alex H <alh311@gmail.com>
2022-09-13 21:50:46 -0600

    Ensure all item pages have loaded

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
+// Craigslist's auth page.
 const loginUrl = 'https://accounts.craigslist.org/login';
 
 // This can be set to 'display' for debugging purposes.
 const buttonValue = 'display';
+
+// The amount of time to wait for all pages to load after renewing items.
+const pageLoadWaitMs = 10000;
 
 (async () => {
     await run();
@@ -45,10 +49,13 @@ async function run() {
     logPage(await page.title());
     const renewButtons = await page.$$(`input[value="${buttonValue}"][type="submit"]`);
 
+
     let count = 0;
+    let pageCount = 1;
     await page.keyboard.down('Shift');
     for (const button of renewButtons) {
         await button.click();
+        ++pageCount;
         ++count;
         if (count >= 5) {
             break;
@@ -56,10 +63,8 @@ async function run() {
     }
     await page.keyboard.up('Shift');
 
-    // TODO: find a better way to wait on the pages to open before asking browser for all the pages.
-    await sleep(1000);
-
-    itemPages = (await browser.pages()).filter(p => p != page);
+    itemPages = (await getAllPages(browser, pageCount))
+        .filter(p => p != page);
 
     console.log(`Pages: ${itemPages.length}`)
     for (const p of itemPages) {
@@ -75,4 +80,19 @@ function logPage(title) {
 
 function sleep(ms) {
     return new Promise(r => setTimeout(r, ms));
+}
+
+async function getAllPages(browser, expectedPageCount) {
+    const startTime = new Date();
+    let currentTime;
+    let pages;
+
+    // Continually get the pages in the browser until we have all of them.
+    // To prevent an infinite loop, set a time limit and return all pages obtained at that point if the time limit is met.
+    do {
+        pages = await browser.pages();
+        currentTime = new Date();
+    } while (pages.length < expectedPageCount && currentTime - startTime <= pageLoadWaitMs)
+
+    return pages;
 }
